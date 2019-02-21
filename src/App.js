f10442ab0a3c0e4246af60a3314a3fd5c9e83f71
ibaczewska <izabela.baczewska@gmail.com>
2019-02-21 01:03:40 +0100

    added background and header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { hot } from 'react-hot-loader';
 import React, { Component } from 'react';
+import './styles/theme.sass';
 
 class App extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <div className="App">
-        <h1>Hi</h1>
+        <h1 className="App__header">Mountain kingdom</h1>
       </div>
     );
   }

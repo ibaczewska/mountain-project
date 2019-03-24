e51d267c6d26403a1909f665222ddc7abdd066e7
ibaczewska <izabela.baczewska@gmail.com>
2019-03-24 12:09:25 +0100

    gh-pages

--- src/components/Button.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-const Button = ({ name, onClick }) => (
-  <button href={"#"} className="button__item" onClick={() => onClick(name)}>
-    {name}
-  </button>
-)
-
-export default Button
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import hamburger from './js/hamburger'
-
-ReactDOM.render(<App />, document.getElementById('root'));
